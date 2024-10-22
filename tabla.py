d34fc8ef4fd3e773c384d638fef16da0b82b6f8e
raphinhaesomarcourtz11 <guareno.resina.samuel@iescamas.es>
2024-10-22 10:56:12 +0200

    Add files via upload

---
+++ tabla.py
@@ -0,0 +1,8 @@
+print("dime el numero de la tabla multiplicar:")
+num_tabla = int(input())
+indice=1
+while(indice < 11):
+    resultado = num_tabla * indice 
+    print(indice , "x",num_tabla,"=", resultado  )
+    indice=indice +1
+    print("FIN")
